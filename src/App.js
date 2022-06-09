5a51e497748e5b837ad6739965e56b7b3ab07a66
iDeerBoy <deerboy.art@gmail.com>
2022-06-09 17:56:13 -0500

    Creando context y llamadas a la api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
-import './App.scss';
-import { ContextApp } from './context/ContextApp';
-import { useContext } from 'react';
+import logo from "./logo.svg";
+import "./App.scss";
+import { ContextApp } from "./context/ContextApp";
+import { useContext } from "react";
 
 function App() {
-  const {games} = useContext(ContextApp)
-  console.log(games);
+  const { games } = useContext(ContextApp);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {games.map((game, i) => {
+        return <p key={i}>{game.title}</p>;
+      })}
     </div>
   );
 }
