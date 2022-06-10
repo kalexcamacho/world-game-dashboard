cf47b6a164ab488146c6933e3024c585ccd6cfe7
CristianKB89 <cristianiksl@gmail.com>
2022-06-09 19:02:11 -0500

    Fix: Cristian Home

---
+++ src/views/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+        <h1>Dashboard de administrador</h1>
+        <section>
+            <h2>Ultimo usuario creado</h2>
+        </section>
+    </section>
+  )
+}
+
+export default Home
