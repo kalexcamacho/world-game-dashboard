f5fdd91d1ee98f8441ee2198f309fe7ccc6322dd
CristianKB89 <cristianiksl@gmail.com>
2022-06-16 16:48:45 -0500

    Home Dashboard

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,58 +1,92 @@
 import React from "react";
 import "./Home.scss";
+import { useContext } from "react";
+import { ContextApp } from "../../context/ContextApp";
 
 const Home = () => {
+  const { users, games, posts, library } = useContext(ContextApp);
+  const lastUser = users[users.length - 1];
+  const userGames = library.filter((game) => game.userId === lastUser.id);
+  const userPosts = posts.filter((post) => post.userId === lastUser.id);
+  const lastGame = games[games.length - 1];
+
+  console.log(lastGame);
   return (
     <section className="Home-wrapper">
       <h1>Dashboard de administrador</h1>
       <section className="Section-wrapper">
-        <p>Ultimo usuario creado</p>
+        <p className="title-mobile-1">Ultimo usuario creado</p>
         <article className="Section-user">
           <div className="User-img">
             <img src="/images/usersProfileImages/1653403428106.jpg" alt="" />
           </div>
           <div className="User-data">
-            <h2>Usuario</h2>
-            <h2>Nickname</h2>
-            <h3>Nombre usuario</h3>
-            <p>email@email.com</p>
+            <h2>{lastUser?.name}</h2>
+            <h3>{lastUser?.nickName}</h3>
+            <p>{lastUser?.email}</p>
             <p>Creado:</p>
-            <p>dd/mm/aaaa</p>
+            <p>{lastUser?.updated_at}</p>
             <div className="Section-data">
               <div className="Section-data-left">
                 <p>No. de juegos</p>
-                <p className="Section-data-number">000</p>
+                <p className="Section-data-number">{userGames?.length}</p>
               </div>
               <div>
                 <p>No. de post</p>
-                <p className="Section-data-number">000</p>
+                <p className="Section-data-number">{userPosts?.length}</p>
               </div>
             </div>
           </div>
         </article>
-        <p>Ultimo juego agregado</p>
+        <p className="title-mobile-2">Ultimo juego agregado</p>
         <article className="Section-user">
           <div className="Game-img">
             <img src="/images/products/gtaV.jpg" alt="" />
           </div>
           <div className="User-data">
-            <h2>Nombre del juego</h2>
-            <h3>Genero</h3>
-            <p className="Section-user-price">$$1231$$</p>
-            <p className="Section-user-price">$$Descuento</p>
-            <p>Xbox</p>
-            <p>PC</p>
-            <p>Play Station</p>
+            <h2>{lastGame?.title}</h2>
+            <h3>{lastGame?.genre.title}</h3>
+            <p className="Section-user-price">${lastGame?.price}</p>
+            <p className="Section-user-price">{lastGame?.discount}%</p>
+            <div className="plataforms-logos">
+              {lastGame?.xbox === 1 && (
+                <img
+                  className="plataforms"
+                  src="/images/platforms/xbox.png"
+                  alt=""
+                />
+              )}
+              {lastGame?.pc === 1 && (
+                <img
+                  className="plataforms"
+                  src="/images/platforms/pc.png"
+                  alt=""
+                />
+              )}
+              {lastGame?.playstation === 1 && (
+                <img
+                  className="plataforms"
+                  src="/images/platforms/ps.png"
+                  alt=""
+                />
+              )}
+            </div>
           </div>
         </article>
       </section>
       <section className="Section-estadistics">
-        <p className="numbers">000</p>
-        <p className="names">Total de usuarios</p>
-        <p className="numbers-games">000</p>
-        <p className="names">Total de juegos</p>
-        <p className="numbers">000</p>
-        <p className="names">Total de post</p>
+        <div>
+          <p className="numbers">{users?.length}</p>
+          <p className="names">Total de usuarios</p>
+        </div>
+        <div>
+          <p className="numbers-games">{games?.length}</p>
+          <p className="names">Total de juegos</p>
+        </div>
+        <div>
+          <p className="numbers">{posts?.length}</p>
+          <p className="names">Total de post</p>
+        </div>
       </section>
     </section>
   );
